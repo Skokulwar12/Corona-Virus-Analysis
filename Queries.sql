d8387bfd69fdabeade76f27c3619d962bd8c08f3
Srushti Kokulwar <163553184+Skokulwar12@users.noreply.github.com>
2024-05-29 21:04:04 +0530

    Add files via upload

---
+++ Queries.sql
@@ -0,0 +1,12 @@
+CREATE TABLE 
+			covid_dataset (
+			province VARCHAR(150),
+			country_or_region VARCHAR(200),
+			latitude FLOAT,
+			longitude FLOAT,
+			date DATE,
+			confirmed INT,
+			deaths INT,
+			recovered INT
+);
+
