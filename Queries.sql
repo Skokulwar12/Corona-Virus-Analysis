95cdc37cd92a5e615cb0ba0e1a9d006cae04e7d6
Srushti Kokulwar <163553184+Skokulwar12@users.noreply.github.com>
2024-06-09 19:05:17 +0530

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -38,9 +38,52 @@ GROUP BY Months;
 
 -- Q5. Find most frequent value for confirmed, deaths, recovered each month.
 
-SELECT TO_CHAR(date,'Month') AS Months
-	   MODE(confirmed) AS most_confirmed,
-	   MODE(deaths) AS most_deaths,
-	   MODE(recovered) AS most_recovered
+SELECT 
+	TO_CHAR(date, 'Month') AS Month_name,
+	MODE() WITHIN GROUP (ORDER BY confirmed) AS most_confirmed,
+	MODE() WITHIN GROUP (ORDER BY deaths) AS most_deaths,
+	MODE() WITHIN GROUP (ORDER BY recovered) AS most_recovered
 FROM covid_dataset
-GROUP BY Months;
+WHERE confirmed != '0'
+AND recovered != '0'
+AND deaths != '0'
+GROUP BY Month_name
+ORDER BY most_confirmed DESC, 
+		 most_recovered DESC;
+
+--Q6. Find minimum values for confirmed, deaths, recovered per year.
+
+SELECT TO_CHAR(date, 'YYYY') AS Year,
+	MIN(confirmed) AS min_confirmed,
+	MIN(deaths) AS min_deaths,
+	MIN(recovered) AS min_recovered
+FROM covid_dataset
+WHERE confirmed != '0'
+AND recovered != '0'
+AND deaths != '0'
+GROUP BY Year;
+
+-- Q7. Find maximum values of confirmed, deaths, recovered per year.
+
+SELECT TO_CHAR(date, 'YYYY') AS Year,
+	MAX(confirmed) AS min_confirmed,
+	MAX(deaths) AS min_deaths,
+	MAX(recovered) AS min_recovered
+FROM covid_dataset
+GROUP BY Year;
+
+-- Q8. The total number of case of confirmed, deaths, recovered each month.
+
+SELECT TO_CHAR(date, 'Month') AS Months,
+	   SUM(confirmed) AS total_confirmed,
+	   SUM(deaths) AS total_deaths,
+	   SUM(recovered) AS total_recovered
+FROM covid_dataset
+GROUP BY Months
+ORDER BY total_confirmed DESC,
+		 total_deaths DESC,
+	 	 total_recovered DESC;
+
+-- Q9. Check how corona virus spread out with respect to confirmed case.
+--      (Eg.: total confirmed cases, their average, variance & STDEV )
+
